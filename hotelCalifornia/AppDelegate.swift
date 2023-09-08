fdbe77a0780ef2db1369e237d9f34924d0f1a6e8
Dimash <dbek1405@gmail.com>
2023-09-08 14:28:25 +0600

    Initial Commit

---
+++ hotelCalifornia/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  hotelCalifornia
+//
+//  Created by Dinmukhamed on 08.09.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // MARK: - Window setup
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .white
+        
+        return true
+    }
+
+}
+
---
+++ hotelCalifornia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hotelCalifornia
+//
+//  Created by Dinmukhamed on 08.09.2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
